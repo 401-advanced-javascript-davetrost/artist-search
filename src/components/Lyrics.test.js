38c0f62d296121626fc183a16d6e4762b46c5f72
Dave Trost <davetrost@gmail.com>
2019-11-08 16:04:04 -0800

    lyrics tests

--- src/components/Lyrics.test.js
+++ src/components/Lyrics.test.js
@@ -2,39 +2,41 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Lyrics from './Lyrics';
 
-jest.mock('react-router-dom', () => {
-  return {
-    useParams: () => ({
-      artist: 'Tribe Called Qwest',
-      track: 'phony rappers'
-    }),
-  };
-});
+import reactRouterMock from 'react-router-dom';
+jest.mock('react-router-dom', () => ({}));
+
+import useLyricsMock from '../containers/use-lyrics.js';
+jest.mock('../containers/use-lyrics.js', () => ({}));
+
+reactRouterMock.useParams = () => ({ artist: 'Tribe Called Qwest', track: 'phony rappers' });
 
 describe('Lyrics component', () => {
-  it('renders Lyrics not found', () => {
-    const wrapper = shallow(<Lyrics />);
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  jest.mock('../containers/use-lyrics', () => {
-    return {
-      useLyrics: () => ({ loading: true })
-    };
-  });  
   
   it('renders Loading', () => {
+    useLyricsMock.useLyrics = () => ({ 
+      loading: true 
+    });
     const wrapper = shallow(<Lyrics />);
     expect(wrapper).toMatchSnapshot();
   });
-
-  jest.mock('../services/lyrics-api', () => {
-    return () => 'phony rappers, who do not write, phony rappers, who do not excite';
-  });  
-
+  
   it('renders Lyrics', () => {
+    useLyricsMock.useLyrics = () => ({ 
+      loading: false, 
+      lyrics: 'Phony rappers. Who do not write. Phony rappers. Who do not excite. Phony rappers. Check it out, aight' 
+    });
     const wrapper = shallow(<Lyrics />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('renders Lyrics not found', () => {
+    useLyricsMock.useLyrics = () => ({ 
+      loading: false,
+      lyrics: ''
+    });
+    const wrapper = shallow(<Lyrics />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
 
 });
